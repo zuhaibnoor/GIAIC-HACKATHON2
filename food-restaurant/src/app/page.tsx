ac3ef99241af84639bcf91504da8407d7c805f76
zuhaibnoor <zuhaibnoor5@gmail.com>
2025-01-04 07:08:03 -0500

    added about us section in homepage

--- food-restaurant/src/app/page.tsx
+++ food-restaurant/src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <div
       className="h-screen bg-cover bg-center"
       style={{ 
@@ -13,7 +14,7 @@ export default function Home() {
       }}
     >
       {/* Black overlay */}
-      <div className="bg-[#0D0D0D] opacity-90 h-full w-screen">
+      <div className="bg-[#0D0D0D] opacity-90 h-screen w-screen">
         {/* Title */}
         <div className="text-base font-Helvetica font-bold mx-auto w-max pt-5">
           <div className="text-[#FF9F0D] inline">Food</div>
@@ -108,5 +109,65 @@ export default function Home() {
       
       { /* bg image div*/ }
     </div>  
+    
+    {/* second div start*/}
+    <div className='h-screen w-screen bg-[#0D0D0D] p-5 lg:p-20'>
+
+        {/* flex container for second div start*/}
+        <div className='flex flex-col lg:flex-row'>
+            
+            {/* flex item 1 (about us) start */}
+            <div className='basis-1/2'>
+
+            <h1 className='font-["cursive"] text-[#FF9F0D] text-[32px]'> About us</h1>
+            <div className='text-[#FFFFFF] text-[48px] font-Helvetica leading-tight tracking-tight font-[700]'><div className='text-[#FF9F0D] inline '> We</div> Create the best<br className=''/>foody product</div>
+            <div className='pt-4 lg:pr-10 text-justify mb-7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque diam pellentesque bibendum non dui volutpat fringilla bibendum. Urna, elit augue urna, vitae feugiat pretium donec id elementum. Ultrices mattis sed vitae mus risus. Lacus nisi, et ac dapibus sit eu velit in consequat.</div>
+            
+            <ul className='flex flex-col lg:space-y-7'>
+              <li> <Image src={"/check.svg"} width={17} height={12} alt='list-style' className='inline pr-2'/> Lacus nisi, et ac dapibus sit eu velit in consequat.</li>
+              <li> <Image src={"/check.svg"} width={17} height={12} alt='list-style' className='inline pr-2'/>Lacus nisi, et ac dapibus sit eu velit in consequat.</li>
+              <li> <Image src={"/check.svg"} width={17} height={12} alt='list-style' className='inline pr-2'/> Lacus nisi, et ac dapibus sit eu velit in consequat.</li>
+              <div className='ml-0 lg:mt-5 h-[50px] w-[190px] bg-[#FF9F0D] place-content-center text-center rounded-[30px] text-[16px] font-Inter'><Link href="/About" className='text-center place-content-center'>Read More</Link></div>
+            </ul>
+
+            {/* flex item 1 (about us) end */}
+            </div>
+            
+            
+            {/* flex item 2 for images start */}
+            <div className='basis-1/2   hidden lg:inline'>
+              
+              {/* images 1,2 and 3 here start */}
+              <div className='grid grid-cols-2 gap-2 '>
+                <Image src={"/div2-img1.png"} height={330} width={660} alt="div2-img1.png" className='col-span-2'/>
+                <Image src={"/div2-img2.png"} height={330} width={322} alt="div2-img1.png" className=''/>
+                <Image src={"/div2-img3.png"} height={330} width={322} alt="div2-img1.png" className='ml-[-8%]'/>
+
+              {/* images 1,2 and 3 here start */}
+              </div>
+            
+            {/* flex item 2 for images end */}
+            </div>
+
+        
+        {/* flex container for second div end*/}
+        </div>       
+            
+
+    {/* second div end */}
+    </div>
+
+    {/* third div start (choose food items) */}
+    <div className='h-screen w-screen bg-[#0D0D0D]'>
+
+      <h1 className='font-["cursive"] text-[#FF9F0D] text-[32px] text-center'> Food Category</h1>
+      <div className='text-[#FFFFFF] text-[48px] font-Helvetica leading-tight tracking-tight font-[700] text-center'><div className='text-[#FF9F0D] inline '> Ch</div>oose Food Item</div>
+
+
+
+
+      {/* third div start (choose food items) */} 
+    </div>
+    </>
   );
 }
